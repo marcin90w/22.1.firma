36a885a2713f166d61ec050e5f5896d7c29c8c76
marcin90w <marcin90w@gmail.com>
2022-06-30 17:00:09 +0200

    do oceny

--- src/main/java/com/example/firma/MailController.java
+++ src/main/java/com/example/firma/MailController.java
@@ -1,6 +1,5 @@
 package com.example.firma;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
